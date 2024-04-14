7aa8ab0c3aba436d50a8e20926974bdc709c24ce
thouxanbanunotheactivist <priorityhighh@gmail.com>
2024-04-14 20:03:54 +0300

    done synchronized

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,43 @@
 public class Main {
-    private static volatile boolean isPing = true;
+    private static boolean isPing = true;
     private static int iterNum = 0;
-    
+    private static final Object lock = new Object();
+
     public static void main(String[] args) {
         new Thread(Main::ping).start();
         new Thread(Main::pong).start();
     }
     private static void ping() {
         while (true){
-            if(isPing) {
-                System.out.println(iterNum+ " Ping");
+            synchronized (lock){
+                if(isPing) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                System.out.println(iterNum + " Ping");
                 iterNum++;
-                isPing = false;
+                isPing = true;
+                lock.notify();
             }
         }
     }
     private static void pong() {
         while (true) {
-            if (!isPing) {
-                System.out.println(iterNum + "        Pong");
+            synchronized (lock){
+                if(!isPing) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                System.out.println(iterNum++  + "       Pong");
                 iterNum++;
-                isPing = true;
+                isPing = false;
+                lock.notify();
             }
         }
     }
