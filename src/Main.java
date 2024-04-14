57363d60bf5417818c5f498004c945f3044c4d29
thouxanbanunotheactivist <priorityhighh@gmail.com>
2024-04-14 20:00:56 +0300

    done java.util.concurrent

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,33 @@
+import java.util.concurrent.Semaphore;
+
 public class Main {
-    private static volatile boolean isPing = true;
+    private static boolean isPing = true;
     private static int iterNum = 0;
-    
+    private static Semaphore semaphore = new Semaphore(1, true);
+
     public static void main(String[] args) {
         new Thread(Main::ping).start();
         new Thread(Main::pong).start();
     }
     private static void ping() {
         while (true){
-            if(isPing) {
-                System.out.println(iterNum+ " Ping");
-                iterNum++;
-                isPing = false;
+            try {
+                semaphore.acquire();
+                System.out.println(iterNum++ + " Ping");
+                semaphore.release();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
     }
     private static void pong() {
         while (true) {
-            if (!isPing) {
-                System.out.println(iterNum + "        Pong");
-                iterNum++;
-                isPing = true;
+            try {
+                semaphore.acquire();
+                System.out.println(iterNum++ + "       Pong");
+                semaphore.release();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
     }
